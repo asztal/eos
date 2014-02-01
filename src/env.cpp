5addbddbe6c0038eb58c4a1727e9b1c858619065
Lee Houghton <lee@theconnectgroup.net>
2014-02-01 14:37:00 +0000

    Add Environment.drivers()
    TODO: Documentation

--- src/env.cpp
+++ src/env.cpp
@@ -13,8 +13,9 @@ void Environment::Init(Handle<Object> exports) {
     auto sig0 = Signature::New(constructor_);
 
     EOS_SET_METHOD(constructor_, "newConnection", Environment, NewConnection, sig0);
-    EOS_SET_METHOD(constructor_, "free", Environment, Free, sig0);
     EOS_SET_METHOD(constructor_, "dataSources", Environment, DataSources, sig0);
+    EOS_SET_METHOD(constructor_, "drivers", Environment, Drivers, sig0);
+    EOS_SET_METHOD(constructor_, "free", Environment, Free, sig0);
 
     exports->Set(String::NewSymbol("Environment"), constructor_->GetFunction(), ReadOnly);
 }
@@ -90,7 +91,7 @@ Handle<Value> Environment::DataSources(const Arguments& args) {
         ret = SQLDataSourcesW(
             GetHandle(),
             direction,
-            serverName, sizeof(serverName) / sizeof(wchar_t), &serverNameLength, // TODO not crash
+            serverName, sizeof(serverName) / sizeof(wchar_t), &serverNameLength,
             description, sizeof(description) / sizeof(wchar_t), &descriptionLength);
     
         if (ret == SQL_NO_DATA)
@@ -102,6 +103,55 @@ Handle<Value> Environment::DataSources(const Arguments& args) {
         auto item = Object::New();
         item->Set(String::NewSymbol("server"), StringFromTChar(serverName, min(sizeof(serverName) / sizeof(wchar_t), (size_t)serverNameLength)));
         item->Set(String::NewSymbol("description"), StringFromTChar(description, min(sizeof(description) / sizeof(wchar_t), (size_t)descriptionLength)));
+        results->Set(i++, item);
+
+        direction = SQL_FETCH_NEXT;
+    }
+}
+
+Handle<Value> Environment::Drivers(const Arguments& args) {
+    EOS_DEBUG_METHOD();
+
+    SQLUSMALLINT direction = SQL_FETCH_FIRST;
+
+    auto results = Array::New();
+
+    uint32_t i = 0;
+    SQLRETURN ret;
+
+    const int maxAttributesLength = 4096;
+    wchar_t description[SQL_MAX_DSN_LENGTH + 1], driverAttributes[maxAttributesLength + 1];
+    SQLSMALLINT descriptionLength, driverAttributesLength;
+
+    for(;;) {
+        ret = SQLDriversW(
+            GetHandle(),
+            direction,
+            description, sizeof(description) / sizeof(wchar_t), &descriptionLength,
+            driverAttributes, sizeof(driverAttributes) / sizeof(wchar_t), &driverAttributesLength);
+    
+        if (ret == SQL_NO_DATA)
+            return results;
+
+        if (ret == SQL_ERROR)
+            return ThrowException(GetLastError());
+
+        auto item = Object::New();
+        item->Set(String::NewSymbol("name"), StringFromTChar(description, min(sizeof(description) / sizeof(wchar_t), (size_t)descriptionLength)));
+
+        auto attributes = Array::New();
+        
+        wchar_t* str = driverAttributes;
+        for (uint32_t j = 0;; j++) {
+            auto attribute = StringFromTChar(str);
+            if (attribute->Length() == 0)
+                break;
+
+            attributes->Set(j, attribute);
+            str += wcslen(str) + 1;
+        }
+
+        item->Set(String::NewSymbol("attributes"), attributes);
         results->Set(i++, item);
 
         direction = SQL_FETCH_NEXT;
--- src/env.hpp
+++ src/env.hpp
@@ -14,6 +14,7 @@ namespace Eos {
         // JS methods
         Handle<Value> NewConnection(const Arguments& args);
         Handle<Value> DataSources(const Arguments& args);
+        Handle<Value> Drivers(const Arguments& args);
         Handle<Value> Free(const Arguments& args);
 
     public:
